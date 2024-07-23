752f95ece59c0a9e35893a23b0feab6d1505356c
codingloserfoolish <99189549+codingloserfoolish@users.noreply.github.com>
2024-07-24 02:01:32 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,15 +1,31 @@
 #include<qapplication.h>
+#include"Widget/StartWidget.h"
 #include"Widget/MainWindow.h"
 //#include"Widget/ObjectsTreeWidget.h"
 //LINE_EXTRA:qtvariantproperty.cpp
 
+void myMessageOutput(QtMsgType type, const QMessageLogContext& context, const QString& msg)
+{
+	if (type != QtWarningMsg || !msg.startsWith("QWindowsWindow::setGeometry")) {
+		QByteArray localMsg = msg.toLocal8Bit();
+		fprintf(stdout, localMsg.constData());
+	}
+}
 
 int main(int arg, char** argv)
 {
+	qInstallMessageHandler(myMessageOutput);
 	QApplication a(arg, argv);
-	MainWindow w;
-	w.show();
-	/*ObjectsTreeWidget w({ "name" });
-	w.show();*/
-	return a.exec();
+
+	StartWidget tw;
+	tw.setModal(true);
+	if (QDialog::Accepted == tw.exec())
+	{
+		MainWindow w;
+		w.show();
+		/*ObjectsTreeWidget w({ "name" });
+		w.show();*/
+		return a.exec();
+	}
+	return 0;
 }
